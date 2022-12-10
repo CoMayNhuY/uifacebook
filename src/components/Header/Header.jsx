d0bfabd6e90d4bc0d8682f93ffb02fb8a8711c61
Your Name <btsthanhvancghcnttts@gmail.com>
2022-12-10 21:45:12 +0700

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,37 @@
-import '../../components/Header/header.css';
-import BellHeader from './BellHedeader';
+import "../../components/Header/header.css";
+import BellHeader from "./BellHedeader";
 function Header() {
-    return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-sm-4 colstyle1">
-                    <span className="logoFB">Facebook</span>
-                </div>
-                <div className="col-sm-4 colstyle2">
-                    <div className="input-group">
-                        <input type="text" className="form-control" placeholder="Search..." />
-                        <button className="btn btn-info" type="submit">
-                            <i className="fa-solid fa-magnifying-glass"></i>
-                        </button>
-                    </div>
-                </div>
-                <div className="col-sm-4 colstyle3">
-                    <BellHeader />
-                    <div>
-                        <img
-                            src="https://haycafe.vn/wp-content/uploads/2022/02/Anh-gai-xinh-Viet-Nam.jpg"
-                            alt="avt"
-                            className="imgAvt"
-                        />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm-4 colstyle1">
+          <span className="logoFB">Facebook</span>
         </div>
-    );
+        <div className="col-sm-4 colstyle2">
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search..."
+            />
+            <button className="btn btn-info" type="submit">
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </div>
+        </div>
+        <div className="col-sm-4 colstyle3">
+          <BellHeader />
+          <div>
+            <img
+              src="https://haycafe.vn/wp-content/uploads/2022/02/Anh-gai-xinh-Viet-Nam.jpg"
+              alt="avt"
+              className="imgAvt"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
